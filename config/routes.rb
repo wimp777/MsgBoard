0f19f7cb806ed509f10f3f028cf478b74ed342ff
wimp777 <wimp777@gmail.com>
2016-05-22 13:22:40 -0500

    add comment function to message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   devise_for :views
-  resources :messages
+  resources :messages do
+    resources :comments
+  end
   root 'messages#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522045555) do
+ActiveRecord::Schema.define(version: 20160522175739) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "message_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["message_id"], name: "index_comments_on_message_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
